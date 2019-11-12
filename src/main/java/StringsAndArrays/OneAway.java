1649ba09a3227c745e28f1d08f5249a6dc3bc82c
Diego Romero <dev.diego.romero@gmail.com>
2019-11-12 11:02:11 +0000

    added one away result

--- src/main/java/StringsAndArrays/OneAway.java
+++ src/main/java/StringsAndArrays/OneAway.java
@@ -1,7 +1,55 @@
 package StringsAndArrays;
 
+import java.util.HashMap;
+
 public class OneAway {
-    boolean solution (String s1, String s2) {
-        return false;
+
+
+    /*
+    Diego Solution
+     */
+    boolean diegoSolution(String s1, String s2) {
+
+        if (s1.equals(s2)) return true;
+
+        char[] s1Chars = s1.toCharArray();
+        int s1Length = s1Chars.length;
+        char[] s2Chars = s2.toCharArray();
+        int s2Length = s2Chars.length;
+
+        if ((s2Length < s1Length && s2Length +1 != s1Length)) return false;
+        if ((s2Length > s1Length && s2Length -1 != s1Length)) return false;
+
+        HashMap<Character, Integer> letters = diegoGenerateLettersMap(s1Chars);
+
+        // reduce the counter of each letter based
+        for (int i = 0; i < s2Length; i++) {
+            char current = s2Chars[i];
+            if (letters.get(current) == null) {
+                letters.put(current, 0);
+            } else {
+                int value = letters.get(current);
+                letters.put(current, value - 1);
+            }
+        }
+
+        // gets the count of how many different letters
+        Integer difference = letters.values().stream().reduce(Integer::sum).get();
+        return difference < 2 && difference > -2;
+    }
+
+    HashMap<Character, Integer> diegoGenerateLettersMap(char[] chars) {
+        HashMap<Character, Integer> letters = new HashMap();
+
+        for (int i = 0; i < chars.length; i++) {
+            char current = chars[i];
+            if (letters.get(current) == null) {
+                letters.put(current, 1);
+            } else {
+                int value = letters.get(current);
+                letters.put(current, value + 1);
+            }
+        }
+        return letters;
     }
 }
--- src/test/java/StringsAndArrays/OneAwayTest.java
+++ src/test/java/StringsAndArrays/OneAwayTest.java
@@ -6,22 +6,47 @@ public class OneAwayTest {
     OneAway oneAway = new OneAway();
 
     @Test
+    void validTest0() {
+        assert (oneAway.diegoSolution("pale", "pale"));
+    }
+
+    @Test
     void validTest1() {
-        assert (oneAway.solution("pale", "ple"));
+        assert (oneAway.diegoSolution("pale", "ple"));
     }
 
     @Test
     void validTest2() {
-        assert (oneAway.solution("pales", "pale"));
+        assert (oneAway.diegoSolution("pales", "pale"));
     }
 
     @Test
     void validTest3() {
-        assert (oneAway.solution("pale", "bale"));
+        assert (oneAway.diegoSolution("pale", "bale"));
     }
 
     @Test
-    void invalidTest() {
-        assert (!oneAway.solution("pale", "bake"));
+    void invalidTest0() {
+        assert (!oneAway.diegoSolution("pale", "bake"));
+    }
+
+    @Test
+    void invalidTest1() {
+        assert (!oneAway.diegoSolution("pale", "long string"));
+    }
+
+    @Test
+    void invalidTest2() {
+        assert (!oneAway.diegoSolution("pale", "xy"));
+    }
+
+    @Test
+    void invalidTest3() {
+        assert (!oneAway.diegoSolution("pale", "bal"));
+    }
+
+    @Test
+    void invalidTest4() {
+        assert (!oneAway.diegoSolution("pale", "paleee"));
     }
 }
