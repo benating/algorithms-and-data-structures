46f404570f9dc1ac5f0ea761ab2efc8cb746d1c9
chrislmy <zcabmyl@ucl.ac.uk>
2019-11-19 21:48:37 +0000

    zero matrix solutions

--- src/main/java/StringsAndArrays/ZeroMatrix.java
+++ src/main/java/StringsAndArrays/ZeroMatrix.java
@@ -3,47 +3,81 @@ package StringsAndArrays;
 import java.util.*;
 
 public class ZeroMatrix {
-    public Integer[][] solution(Integer[][] matrix) {
 
-        HashMap<String, ArrayList> cordinates = getZeroCoordinates(matrix);
-        ArrayList<Integer> rows = cordinates.get("rows");
-        ArrayList<Integer> columns = cordinates.get("columns");
+  // O(N^2) time complexity, where N = size of col or row which ever is bigger.
+  // O(MN) space complexity, where M = size of row and N = size of col.
+  public Integer[][] chrisSolution(Integer[][] matrix) {
+    Integer[][] copy = matrix;
+    List<Integer[]> zeroPoints = new ArrayList<>();
+
+    for (int row = 0; row < matrix.length; row++) {
+      for (int col = 0; col < matrix[row].length; col++) {
+        if (matrix[row][col] == 0) {
+          zeroPoints.add(new Integer[]{row, col});
+        }
+      }
+    }
+
+    for (Integer[] point : zeroPoints) {
+      makeRowAndColZero(matrix, point[0], point[1]);
+    }
+
+    return copy;
+  }
+
+  private void makeRowAndColZero(Integer[][] matrix, int row, int col) {
+    for (int i = 0; i < matrix[row].length; i++) {
+      matrix[row][i] = 0;
+    }
+
+    for (int i = 0; i < matrix.length; i++) {
+      matrix[i][col] = 0;
+    }
+
+    return;
+  }
+
+  public Integer[][] solution(Integer[][] matrix) {
+
+    HashMap<String, ArrayList> cordinates = getZeroCoordinates(matrix);
+    ArrayList<Integer> rows = cordinates.get("rows");
+    ArrayList<Integer> columns = cordinates.get("columns");
 //        System.out.println("rows " + rows);
 //        System.out.println("columns " + columns);
 
-        for (int i = 0; i < matrix.length; i++) {
-                Integer[] currentRow = matrix[i];
-            if (rows.contains(i)) {
-                Integer[] newRow = new Integer[currentRow.length];
-                Arrays.fill(newRow, 0);
-                matrix[i] = newRow;
-            } else {
-                columns.forEach(x -> {
-                   currentRow[x] = 0;
-                });
-            }
-        }
-
-        return matrix;
+    for (int i = 0; i < matrix.length; i++) {
+      Integer[] currentRow = matrix[i];
+      if (rows.contains(i)) {
+        Integer[] newRow = new Integer[currentRow.length];
+        Arrays.fill(newRow, 0);
+        matrix[i] = newRow;
+      } else {
+        columns.forEach(x -> {
+          currentRow[x] = 0;
+        });
+      }
     }
 
-    HashMap<String, ArrayList>getZeroCoordinates (Integer[][] matrix) {
-        HashMap<String, ArrayList> coordinates = new HashMap<>();
-        ArrayList rows = new ArrayList<Integer>();
-        ArrayList columns = new ArrayList<Integer>();
+    return matrix;
+  }
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                Integer current = matrix[i][j];
-                if (current == 0) {
-                    rows.add(i);
-                    columns.add(j);
-                }
-            }
+  HashMap<String, ArrayList> getZeroCoordinates(Integer[][] matrix) {
+    HashMap<String, ArrayList> coordinates = new HashMap<>();
+    ArrayList rows = new ArrayList<Integer>();
+    ArrayList columns = new ArrayList<Integer>();
+
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+        Integer current = matrix[i][j];
+        if (current == 0) {
+          rows.add(i);
+          columns.add(j);
         }
-
-        coordinates.put("rows", rows);
-        coordinates.put("columns", columns);
-        return coordinates;
+      }
     }
+
+    coordinates.put("rows", rows);
+    coordinates.put("columns", columns);
+    return coordinates;
+  }
 }
--- src/test/java/StringsAndArrays/ZeroMatrixTest.java
+++ src/test/java/StringsAndArrays/ZeroMatrixTest.java
@@ -5,105 +5,141 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 public class ZeroMatrixTest {
-    ZeroMatrix zeroMatrix = new ZeroMatrix();
 
-    @Test
-    void validTest1() {
-        Integer[][] exampleMatrix = {
-                {1,2,3},
-                {4,5,6},
-                {7,8,0},
-        };
+  ZeroMatrix zeroMatrix = new ZeroMatrix();
 
-        Integer[][] exampleResponse = {
-                {1,2,0},
-                {4,5,0},
-                {0,0,0},
-        };
+  @Test
+  void validTest1() {
+    Integer[][] exampleMatrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 0},
+    };
 
-        Integer[][] solution = zeroMatrix.solution(exampleMatrix);
+    Integer[][] exampleMatrixCopy = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 0},
+    };
 
-        for (Integer[]e : solution) {
-            System.out.println(Arrays.toString(e));
-        }
-        assert (Arrays.deepEquals(solution, exampleResponse));
+    Integer[][] exampleResponse = {
+        {1, 2, 0},
+        {4, 5, 0},
+        {0, 0, 0},
+    };
+
+    Integer[][] solution = zeroMatrix.solution(exampleMatrix);
+    Integer[][] chrisSolution = zeroMatrix.chrisSolution(exampleMatrixCopy);
+
+    for (Integer[] e : chrisSolution) {
+      System.out.println(Arrays.toString(e));
+    }
+    assert (Arrays.deepEquals(solution, exampleResponse));
+    assert (Arrays.deepEquals(chrisSolution, exampleResponse));
+  }
+
+  @Test
+  void validTest2() {
+    Integer[][] exampleMatrix = {
+        {0, 2, 3},
+        {4, 5, 6},
+        {7, 8, 0},
+    };
+
+    Integer[][] exampleMatrixCopy = {
+        {0, 2, 3},
+        {4, 5, 6},
+        {7, 8, 0},
+    };
+
+    Integer[][] exampleResponse = {
+        {0, 0, 0},
+        {0, 5, 0},
+        {0, 0, 0},
+    };
+
+    Integer[][] solution = zeroMatrix.solution(exampleMatrix);
+    Integer[][] chrisSolution = zeroMatrix.chrisSolution(exampleMatrixCopy);
+
+    for (Integer[] e : solution) {
+      System.out.println(Arrays.toString(e));
     }
 
-    @Test
-    void validTest2() {
-        Integer[][] exampleMatrix = {
-                {0,2,3},
-                {4,5,6},
-                {7,8,0},
-        };
+    assert (Arrays.deepEquals(solution, exampleResponse));
+    assert (Arrays.deepEquals(chrisSolution, exampleResponse));
+  }
 
-        Integer[][] exampleResponse = {
-                {0,0,0},
-                {0,5,0},
-                {0,0,0},
-        };
+  @Test
+  void validTest3() {
+    Integer[][] exampleMatrix = {
+        {1, 2, 0, 10},
+        {4, 5, 6},
+        {7, 8, 0, 9},
+    };
 
-        Integer[][] solution = zeroMatrix.solution(exampleMatrix);
+    Integer[][] exampleMatrixCopy = {
+        {1, 2, 0, 10},
+        {4, 5, 6},
+        {7, 8, 0, 9},
+    };
+    for (Integer[] x : exampleMatrix) {
+      System.out.println(Arrays.toString(x));
+    }
+    System.out.println("----------");
 
-        for (Integer[]e : solution) {
-            System.out.println(Arrays.toString(e));
-        }
+    Integer[][] exampleResponse = {
+        {0, 0, 0, 0},
+        {4, 5, 0},
+        {0, 0, 0, 0},
+    };
 
-        assert (Arrays.deepEquals(solution, exampleResponse));
+    Integer[][] solution = zeroMatrix.solution(exampleMatrix);
+    Integer[][] chrisSolution = zeroMatrix.chrisSolution(exampleMatrixCopy);
+
+    for (Integer[] e : solution) {
+      System.out.println(Arrays.toString(e));
     }
 
-    @Test
-    void validTest3() {
-        Integer[][] exampleMatrix = {
-                {1,2,0, 10},
-                {4,5,6},
-                {7,8,0, 9},
-        };
-        for (Integer[] x: exampleMatrix) {
-            System.out.println(Arrays.toString(x));
-        }
-        System.out.println("----------");
+    assert (Arrays.deepEquals(solution, exampleResponse));
+    assert (Arrays.deepEquals(chrisSolution, exampleResponse));
+  }
 
-        Integer[][] exampleResponse = {
-                {0,0,0, 0},
-                {4,5,0},
-                {0,0,0, 0},
-        };
+  @Test
+  void validTest4() {
+    Integer[][] exampleMatrix = {
+        {1, 2, 3},
+        {0, 5, 6},
+        {7, 8, 0},
+        {1, 0, 1},
+    };
 
-        Integer[][] solution = zeroMatrix.solution(exampleMatrix);
+    Integer[][] exampleMatrixCopy = {
+        {1, 2, 3},
+        {0, 5, 6},
+        {7, 8, 0},
+        {1, 0, 1},
+    };
+    for (Integer[] x : exampleMatrix) {
+      System.out.println(Arrays.toString(x));
+    }
+    System.out.println("----------");
 
-        for (Integer[]e : solution) {
-            System.out.println(Arrays.toString(e));
-        }
+    Integer[][] exampleResponse = {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0},
+    };
 
-        assert (Arrays.deepEquals(solution, exampleResponse));
+    Integer[][] solution = zeroMatrix.solution(exampleMatrix);
+    Integer[][] chrisSolution = zeroMatrix.chrisSolution(exampleMatrixCopy);
+
+    for (Integer[] e : solution) {
+      System.out.println(Arrays.toString(e));
     }
 
-    @Test
-    void validTest4() {
-        Integer[][] exampleMatrix = {
-                {1,2,3, 10},
-                {0,5,6},
-                {7,8,0, 9},
-        };
-        for (Integer[] x: exampleMatrix) {
-            System.out.println(Arrays.toString(x));
-        }
-        System.out.println("----------");
-
-        Integer[][] exampleResponse = {
-                {0,2,0, 10},
-                {0,0,0},
-                {0,0,0, 0},
-        };
-
-        Integer[][] solution = zeroMatrix.solution(exampleMatrix);
-
-        for (Integer[]e : solution) {
-            System.out.println(Arrays.toString(e));
-        }
-
-        assert (Arrays.deepEquals(solution, exampleResponse));
-    }
+    assert (Arrays.deepEquals(solution, exampleResponse));
+    assert (Arrays.deepEquals(chrisSolution, exampleResponse));
+  }
 
 }
